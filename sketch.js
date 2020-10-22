119ce07f8e4516fd078bf54d8f33d7aa919b5809
viv_3 <vibhatibindra@gmail.com>
2020-10-22 16:14:04 -0700

    platform added

--- sketch.js
+++ sketch.js
@@ -14,10 +14,10 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+    platform = new Ground(150, 305, 300, 170);
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
